935e64fd1bf2e76fda67aca5678e2a6494326d54
DuarteB <eduarte.dev@gmail.com>
2022-04-11 20:14:21 -0300

    Finishing the Binary Search

--- BinarySearch/src/Search/Code/App.java
+++ BinarySearch/src/Search/Code/App.java
@@ -1,41 +1,33 @@
 package Search.Code;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
-
-
 class BinarySearch {
-	int binarySearch(int arr[], int l, int r, int x)
+	int binarySearch(int arr[], int left, int right, int x) {
+		if (right >= left) {
+			int mid = left + (right - left) / 2;
 
-	{
-		if (r >= l) {
-			int mid = l + (r - l) / 2;
+ 			if (arr[mid] == x) { return mid; }
 
-			if (arr[mid] == x)
-				return mid;
+			if (arr[mid] > x) { return binarySearch(arr, left, mid - 1, x); }
 
-			if (arr[mid] > x)
-				return binarySearch(arr, l, mid - 1, x);
-
-			return binarySearch(arr, mid + 1, r, x);
+			return binarySearch(arr, mid + 1, right, x);
 		}
 
 		return -1;
 	}
+
 	public static void main(String args[])
 	{
 		BinarySearch ob = new BinarySearch();
+
 		int arr[] = { 2, 3, 4, 10, 40 };
 		int n = arr.length;
 		int x = 10;
 		int result = ob.binarySearch(arr, 0, n - 1, x);
-		if (result == -1)
+
+		if (result == -1) {
 			System.out.println("Element not present");
-		else
-			System.out.println("Element found at index "
-							+ result);
+		} else {
+			System.out.println("Element found at index " + result);
+		}
 	}
 }
